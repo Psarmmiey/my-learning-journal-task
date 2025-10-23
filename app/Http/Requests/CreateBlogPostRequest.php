89e453671606b6f1b5635524bb86e67729bc42dd
shanaya-Gupta <shanaya-Gupta@users.noreply.github.com>
2025-10-23 11:15:38 +0000

    fix: Resolve #231 - Add Markdown support for journal entries

--- app/Http/Requests/CreateBlogPostRequest.php
+++ app/Http/Requests/CreateBlogPostRequest.php
@@ -9,6 +9,21 @@ use Illuminate\Foundation\Http\FormRequest;
 class CreateBlogPostRequest extends FormRequest
 {
     /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        // Convert an empty string for body_raw to null. This allows the 'nullable'
+        // rule to pass if a photo is present, while still allowing the
+        // 'required_without' rule to fail correctly if no photo is present.
+        if ($this->get('body_raw') === '') {
+            $this->merge([
+                'body_raw' => null,
+            ]);
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
@@ -17,11 +32,11 @@ class CreateBlogPostRequest extends FormRequest
     {
         return [
             'title' => ['required', 'string', 'max:255'],
-            'excerpt' => ['sometimes', 'required_without:body', 'string'],
-            'body' => ['required_without:photo', 'string'],
+            'excerpt' => ['sometimes', 'required_without:body_raw', 'string'],
+            'body_raw' => ['required_without:photo', 'nullable', 'string', 'min:1'],
             'is_published' => ['required', 'boolean'],
             'is_featured' => ['required', 'boolean'],
-            'photo' => ['required', 'image'],
+            'photo' => ['required_without:body_raw', 'nullable', 'image'],
             'tags' => ['sometimes', 'array'],
             'tags.*' => ['string', 'max:50'],
         ];
